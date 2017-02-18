6a5594bcd20189f4451de6367244ce2cdf270300
Bằng Ngô <Bng Ng>
2017-02-18 22:30:26 +0700

    Vòng lặp for of

---
+++ dist/lesson16.js
@@ -0,0 +1,14 @@
+var colorName = ['Red', 'Green', 'Blue', 'Yellow', 'Puple'];
+for (var index in colorName) {
+    console.log(colorName[index]);
+}
+for (var _i = 0, colorName_1 = colorName; _i < colorName_1.length; _i++) {
+    var item = colorName_1[_i];
+    console.log(item);
+}
+var name = "TEDU";
+for (var _a = 0, name_1 = name; _a < name_1.length; _a++) {
+    var c_1 = name_1[_a];
+    console.log(c_1);
+}
+//# sourceMappingURL=lesson16.js.map
---
+++ lesson16.ts
@@ -0,0 +1,13 @@
+var colorName = ['Red', 'Green', 'Blue', 'Yellow', 'Puple'];
+for(let index in colorName){
+    console.log(colorName[index]);
+}
+
+for(let item of colorName){
+    console.log(item);
+}
+
+let name = "TEDU";
+for(let c of name){
+    console.log(c);
+}
